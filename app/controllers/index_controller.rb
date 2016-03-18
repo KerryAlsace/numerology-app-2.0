b2cab17c712aa1b6da7dcda3fb6cc64c24f90010
KerryAlsace <kerrydougherty@gmail.com>
2016-03-18 15:39:20 -0400

    modified person

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 
-def determine_birthpath_number(birthdate)
+def get_birth_path_num(birthdate)
   number = birthdate[0].to_i + birthdate[1].to_i + birthdate[2].to_i + birthdate[3].to_i + birthdate[4].to_i + birthdate[5].to_i + birthdate[6].to_i + birthdate[7].to_i
   number = number.to_s
   number = number[0].to_i + number[1].to_i
@@ -68,7 +68,7 @@ end
 post '/' do
   birthdate = params[:birthdate].gsub("-", "")
   if valid_birthdate(birthdate)
-    birth_path_num = determine_birthpath_number(params[:birthdate])
+    birth_path_num = get_birth_path_num(params[:birthdate])
     redirect "/message/#{birth_path_num}"
   else
     @error = "Your input wasn't valid, try again using MMDDYYY format!"
