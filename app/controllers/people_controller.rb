90ac4d544631b4c9046c98c6298295b034ef6953
KerryAlsace <kerrydougherty@gmail.com>
2016-03-21 13:52:01 -0400

    edit

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,11 +1,26 @@
-get '/people' do
+get '/' do
   @people = Person.all
   erb :"/people/index"
 end
 
-get '/people/:id' do
+get '/new' do
+  erb :"/people/new"
+end
+
+get '/:id' do
   @person = Person.find(params[:id])
   birth_path_num = Person.get_birth_path_num(@person.birthdate.strftime("%m%d%Y"))
   @message = Person.get_message(birth_path_num)  
   erb :"/people/show"
 end
+
+post '/people'
+  if params[:birthdate].include?("-")
+    birthdate = params[:birthdate]
+  else
+    birthdate = Date.strptime(params[:birthdate], "%m%d%Y")
+  end
+
+  person = Person.create(first_name: params[:first_name], last_name: params[:last_name], birthdate: params[:birthdate])
+    redirect "/people/#{person.id}"
+end 
